60108d31f9658de7880c707a25e4decb3b4f570e
CobbyElsonfx <andohfrancis9187@gmail.com>
2023-10-13 12:49:49 +0000

    removed some codes to data.sql

--- queries.sql
+++ queries.sql
@@ -49,17 +49,6 @@ WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
 
 -- ---day 2 -------
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species)
-VALUES
-    ('Charmander', '2020-02-08', -11, false, 0, 'Fire'),
-    ('Plantmon', '2021-11-15', -5.7, true, 2, 'Grass'),
-    ('Squirtle', '1993-04-02', -12.13, false, 3, 'Water'),
-    ('Angemon', '2005-06-12', -45, true, 1, 'Angel'),
-    ('Boarmon', '2005-06-07', 20.4, true, 7, 'Mammal'),
-    ('Blossom', '1998-10-13', 17, true, 3, 'Plant'),
-    ('Ditto', '2022-05-14', 22, true, 4, 'Unknown');
-
-
 
 -- Inside a transaction update the animals 
 -- table by setting the species column 
